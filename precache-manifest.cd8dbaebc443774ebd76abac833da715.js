ca9d493ffeab1159688c9d452d9ab202fc513ecd
David Banks <david.banks.engineer@gmail.com>
2020-08-18 21:40:14 -0400

    Updates

---
+++ precache-manifest.cd8dbaebc443774ebd76abac833da715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e050b219be2f26b81b23f4756f9982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc05ff072df14d79853",
+    "url": "/static/css/main.bc8a896f.chunk.css"
+  },
+  {
+    "revision": "5c57b0b294c53969f51a",
+    "url": "/static/js/2.8eeb9bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8eeb9bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc05ff072df14d79853",
+    "url": "/static/js/main.b0abb76b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
